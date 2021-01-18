bc78f927db19644147b0044024e9d812ab0d3760
Cooper <cooperwhite14amv@gmail.com>
2021-01-18 13:00:03 -0600

    Started js

---
+++ app.js
@@ -0,0 +1,13 @@
+// HTML Elements
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell')
+
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+
+
+
+// event listeners
